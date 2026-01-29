f788ade55f6fb6cff0e4cfe2deef3f351c377775
Stephen <schroeps@otc.edu>
2026-01-29 12:53:29 -0600

    Initial Commit

---
+++ Unit_1/mysite/blog/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import post
+
+
+@admin.register(post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    populated_fields = {'slug':('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    show_facets = admin.ShowFacets.ALWAYS
+
+# Register your models here.
---
+++ Unit_1/mysite/blog/models.py
@@ -0,0 +1,59 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
+
+class post(models.Model):
+    class status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=status,
+        default=status.DRAFT
+    )
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
+    class meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+# class favorite_post(models.Model):
+#     pk = models.CompositePrimaryKey(
+#         "user", "post"
+#     )
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#     )
+#     post = models.ForeignKey(
+#         'blog.post',
+#         on_delete=models.CASCADE
+#     )
+#     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
